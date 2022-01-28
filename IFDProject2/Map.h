22cbd871fa03c4ebaee6f91a1698b339857b8137
Garrett Dodd <83469501+gdodd1@users.noreply.github.com>
2022-01-27 23:39:04 -0600

    INITIAL COMMIT

---
+++ IFDProject2/Map.h
@@ -0,0 +1,224 @@
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <stdlib.h>
+
+
+#include "IFDParser.h"
+#include "LinkData.h"
+#include "Area.h"
+
+using namespace std;
+
+struct areaNode
+{
+	Area info;
+	areaNode *u;
+	areaNode *d;
+	areaNode *l;
+	areaNode *r;
+};
+
+class Map{
+	private:
+		vector<areaNode*> areasVec;
+		int areacnt;
+
+		//Here is the instance of IFDParser
+	  	IFDParser parser;
+
+		//Some additional class attributes here
+        vector<LinkData*> linkVec;
+		string nextToken;
+
+		bool ifderr;
+
+	public:
+		Map(){
+			ifderr = false;
+			parser.tokenIt();
+
+			nextToken = parser.getNext();
+
+			if(nextToken == "<game>"){
+				parser.eatToken();
+				nextToken = parser.getNext();
+				while(nextToken != "</game>"){  //consider what happens when run out of tokens && not error state
+					if(nextToken == "<area>"){
+						parser.eatToken();
+						nextToken = parser.getNext();
+						makeArea();
+					}
+					else if(nextToken == "<links>"){
+						parser.eatToken();
+						nextToken = parser.getNext();
+						makeLinks();
+					}
+					else{
+						cout<<"Parse Error Location 1"<<endl;
+						ifderr = true;
+						break;
+					}
+					parser.eatToken();
+					nextToken = parser.getNext();
+				}// while !</game>
+
+				if(ifderr){
+					cout<<" :: ERROR :: IF Document Formatting Error"<<endl;
+					cout<<"Game Loading Failed"<<endl;
+					cout<<"General Area of Parsing Failure:"<<endl;
+					cout<<"-------------------------------------"<<endl;
+					cout<<nextToken<<endl;
+					parser.eatToken();
+					nextToken = parser.getNext();
+					cout<<nextToken<<endl;
+					parser.eatToken();
+					nextToken = parser.getNext();
+					cout<<nextToken<<endl;
+					parser.eatToken();
+					nextToken = parser.getNext();
+					cout<<nextToken<<endl;
+					cout<<"-------------------------------------"<<endl;
+					return;
+				}
+				//cout<<"::Found </game>::"<<endl;
+			}
+			else{
+				cout<<" :: ERROR :: No \"<game>\" tag found."<<endl;
+				cout<<"Game Loading Failed"<<endl;
+			}
+
+			//link up areas
+			linkLinks();
+
+
+		}//constructor
+
+		void makeArea(){
+			int id;
+			int goal;
+			string xstr;
+
+			areaNode* tempNodePtr = new areaNode;
+
+			while(nextToken != "</area>"){
+				if(nextToken == "<desc>"){
+					parser.eatToken();
+					nextToken = parser.getNext();
+
+					tempNodePtr->info.setDescription(nextToken);
+				}
+				else if(nextToken == "<feats>"){
+					parser.eatToken();
+					nextToken = parser.getNext();
+
+					istringstream ss(nextToken);
+          				getline(ss, xstr, ',');
+          				tempNodePtr->info.setID(atoi(xstr.c_str()));
+
+          				getline(ss, xstr, ',');
+					tempNodePtr->info.setGoal(atoi(xstr.c_str()));
+				}
+				else if(nextToken == "</desc>" || nextToken == "</feats>"){
+				  //do nothing
+				}
+				else{
+					cout<<"Parse Error Location 2"<<endl;
+					ifderr = true;
+					break;
+				}
+
+				parser.eatToken();
+				nextToken = parser.getNext();
+			}//while !</area>
+
+			//add area to vector
+			areasVec.push_back(tempNodePtr);
+
+		} //end makeArea()
+
+		void makeLinks(){
+			string xstr;
+			int u, d, l, r;
+			while(nextToken != "</links>"){
+				if(nextToken == "<linkset>"){
+					parser.eatToken();
+			  		nextToken = parser.getNext();
+
+					LinkData* tempLDPtr = new LinkData;
+
+					istringstream ss(nextToken);
+        				getline(ss, xstr, ',');
+        				u = atoi(xstr.c_str());
+        				getline(ss, xstr, ',');
+        				d = atoi(xstr.c_str());
+					getline(ss, xstr, ',');
+        				l = atoi(xstr.c_str());
+					getline(ss, xstr, ',');
+					r = atoi(xstr.c_str());
+
+					tempLDPtr->setDigits(u, d, l, r);
+          				linkVec.push_back(tempLDPtr);
+				}
+				else if(nextToken == "</linkset>"){
+					//do nothing
+				}
+				else{
+					cout<<"Parse Error Location 3"<<endl;
+					ifderr = true;
+					break;
+				}
+				parser.eatToken();
+			  	nextToken = parser.getNext();
+			}//while !</links>
+		}//makeLinks()
+
+
+		void linkLinks(){
+			for(int i=0; i<linkVec.size(); i++){
+					areasVec[i]->u = areasVec[(linkVec[i]->getDigit(0))-1];
+					areasVec[i]->d = areasVec[(linkVec[i]->getDigit(1))-1];
+					areasVec[i]->l = areasVec[(linkVec[i]->getDigit(2))-1];
+					areasVec[i]->r = areasVec[(linkVec[i]->getDigit(3))-1];
+			}
+		}
+
+
+		areaNode* getStart(){
+			return areasVec[0];  //returns a pointer to the first area listed
+		}
+
+		friend ostream& operator<<(ostream& os, Map& map){
+			os<<"******************************************************************"<<endl;
+			os<<"CHEATING!!!! Printing the set of areas and connections on the map."<<endl;
+			os<<"******************************************************************"<<endl;
+			for(int i=0; i<map.areasVec.size(); i++){
+				os<<"This is area: "<<i+1<<endl;
+				os<<map.areasVec[i]->info.getDescription()<<endl;
+				if(map.areasVec[i]->info.getID() == 1){
+					os<<"Area is INSTADEATH."<<endl;
+				}
+				if(map.areasVec[i]->info.getGoal() == 1){
+					os<<"Area is GOAL."<<endl;
+				}
+				os<<"Connections:"<<endl;
+				os<<"\t u: Area #"<<map.reverseLookUp(map.areasVec[i]->u)<<endl;
+				os<<"\t d: Area #"<<map.reverseLookUp(map.areasVec[i]->d)<<endl;
+				os<<"\t l: Area #"<<map.reverseLookUp(map.areasVec[i]->l)<<endl;
+				os<<"\t r: Area #"<<map.reverseLookUp(map.areasVec[i]->r)<<endl;
+			}
+			return os;
+		}
+
+		int reverseLookUp(areaNode* addy){
+			for(int i=0; i<areasVec.size(); i++){
+				if(areasVec[i] == addy){
+					return i+1;
+				}
+			}
+			//this should not happen
+			return -1;
+		}
+};
